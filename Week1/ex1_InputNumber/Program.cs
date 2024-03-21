0be509ce2f1fb5c2a96f01a0e3163e92eaf5ac70
Tommy <92947944+tommyHellraiser@users.noreply.github.com>
2024-03-20 23:41:23 -0300

    Add files via upload

---
+++ Week1/ex1_InputNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ex1_InputNumber
+{
+    class Program
+    {
+        static void Main()
+        {
+            Program program = new Program();
+            program.InputNumber();
+        }
+
+        private void InputNumber()
+        {
+            Console.WriteLine("Input a positive number");
+
+            string input = Console.ReadLine() ?? "Null input";
+
+            try
+            {
+                int number = int.Parse(input);
+
+                Console.WriteLine($"The input number is: {number}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error parsing input value. Exception: {e.GetType()} -> {e.Message}");
+            }
+        }
+    }
+}
